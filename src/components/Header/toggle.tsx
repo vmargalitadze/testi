6f6535b54de39367ef3fe39722c460f5bc46c079
vaqsii <vaqsii23@gmail.com>
2025-06-06 22:53:34 +0400

    color theme

--- src/components/Header/toggle.tsx
+++ src/components/Header/toggle.tsx
@@ -1,65 +1,59 @@
-"use client"
-import { Button } from '@/components/ui/button';
-import {   DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuContent,
-    DropdownMenuCheckboxItem,} from '@/components/ui/dropdown-menu'
-import { SunMoon, MoonIcon, SunIcon } from 'lucide-react';
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem,
+} from "@/components/ui/dropdown-menu";
+import { SunMoon, MoonIcon, SunIcon } from "lucide-react";
 
-import { useTheme } from 'next-themes'
-import React, { useEffect, useState } from 'react'
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
 
 function Toggle() {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
-  
-    useEffect(() => {
-      setMounted(true);
-    }, []);
-  
-    if (!mounted) {
-      return null;
-    }
-  
-    return (
-      <DropdownMenu >
-        <DropdownMenuTrigger  asChild>
-          <Button
-            variant='ghost'
-            className='focus-visible:ring-0 focus-visible:ring-offset-0 cursor-pointer'
-          >
-            {theme === 'system' ? (
-              <SunMoon  className='cursor-pointer'/>
-            ) : theme === 'dark' ? (
-              <MoonIcon className='cursor-pointer' />
-            ) : (
-              <SunIcon className='cursor-pointer' />
-            )}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>Appearance</DropdownMenuLabel>
-          <DropdownMenuSeparator />
- 
-          <DropdownMenuCheckboxItem
-          className='cursor-pointer'
-            checked={theme === 'dark'}
-            onClick={() => setTheme('dark')}
-          >
-            Dark
-          </DropdownMenuCheckboxItem>
-          <DropdownMenuCheckboxItem
-          className='cursor-pointer'
-            checked={theme === 'light'}
-            onClick={() => setTheme('light')}
-          >
-            Light
-          </DropdownMenuCheckboxItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="w-5 cursor-pointer h-5 text-white flex items-center justify-center p-0 m-0 bg-transparent">
+          {theme === "dark" ? (
+            <MoonIcon className="w-5 h-5 text-white" />
+          ) : (
+            <SunIcon className="w-5 h-5 text-white" />
+          )}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuCheckboxItem
+          className="cursor-pointer"
+          checked={theme === "dark"}
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          className="cursor-pointer"
+          checked={theme === "light"}
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
-export default Toggle
+export default Toggle;
