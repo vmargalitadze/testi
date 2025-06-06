dbb97ddd432f351130e5e3dc9d5422cbf4d0cf3f
vaqsii <vaqsii23@gmail.com>
2025-06-06 21:16:54 +0400

    again

--- src/components/Api/Api.tsx
+++ src/components/Api/Api.tsx
@@ -29,16 +29,16 @@ function Api() {
   return (
     <>
       <div className="mt-24 mb-14">
-        <section className="mx-auto flex flex-col container items-center px-4">
-          <h2 className="text-xl text-black 2xl:text-3xl font-bold mb-6 text-center mt-8">
-            პროდუქტები
+        <section className="mx-auto flex flex-col items-center px-4 max-w-7xl">
+          <h2 className="text-2xl text-black lg:text-4xl 2xl:text-6xl font-bold mb-6 text-center mt-8">
+            Products
           </h2>
 
-          <div className="w-full  grid sm:grid-cols-2 lg:grid-cols-4 gap-6 place-items-center">
+          <div className="w-full flex flex-wrap gap-6 justify-center">
             {products.map((product) => (
               <div
                 key={product.id}
-                className="p-8 border-2 border-gray-200 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full "
+                className="p-8 border-2 border-gray-200 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out w-full max-w-xs"
               >
                 <div className="w-full h-64 mb-4 relative">
                   <Link href={`/products/${product.id}`}>
@@ -53,12 +53,12 @@ function Api() {
 
                 <div className="flex flex-col text-center justify-between">
                   <Link href={`/products/${product.id}`}>
-                    <h3 className="font-sans text-[16px] font-bold">
+                    <h3 className="font-sans text-[24px] font-bold">
                       {product.name}
                     </h3>
                   </Link>
-                  <span className="text-[15px] mt-2 font-bold font-mono">
-                 price:   ${Number(product.price).toFixed(2)}
+                  <span className="text-[20px] mt-2 font-bold font-mono">
+                    price: ${Number(product.price).toFixed(2)}
                   </span>
                 </div>
               </div>
