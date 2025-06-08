d47f25d41811f4bf5201635e4259d0bbb4c7bc0f
vaqsii <vaqsii23@gmail.com>
2025-06-08 15:49:48 +0400

    link

--- src/app/cart/CartItem.tsx
+++ src/app/cart/CartItem.tsx
@@ -20,6 +20,9 @@ const CartItemCard = ({ cartItem }: Props) => {
     <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
       <div className="flex w-2/5">
         <div className="w-20">
+        <Link href={`/product/${cartItem.product.id}`}>
+
+
           <Image
             width={80}
             height={96}
@@ -27,6 +30,7 @@ const CartItemCard = ({ cartItem }: Props) => {
             src={cartItem.product.images[0]}
             alt={cartItem.product.name}
           />
+          </Link>
         </div>
         <div className="flex flex-col justify-between ml-4 flex-grow">
           <span className="font-bold text-sm">{cartItem.product.name}</span>
