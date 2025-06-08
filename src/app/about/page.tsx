a0bfcf81473dad5077549c8a3b80746ce1d53cce
vaqsii <vaqsii23@gmail.com>
2025-06-08 21:35:25 +0400

    about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,41 +3,40 @@ import Image from "next/image";
 function page() {
   return (
     <>
-      <div className="pt-[70px] rounded-md  pb-[70px]">
+      <div className="mt-[40px] rounded-md pb-[70px]">
         <div className="container">
-          <div className="max-w-[1720px] mx-auto grid grid-cols-1 lg:grid-cols-2 ">
-            <div className="md:w-[550px] w-full  md:h-full h-[200px]  relative">
+          <div className="max-w-[1720px] mx-auto grid grid-cols-1 lg:grid-cols-2">
+            {/* Image container */}
+            <div className="relative w-full h-[550px]  ">
               <Image
-                fill
-                className=" rounded-md"
-                alt="about"
                 src="/slider/1.jpg"
+                alt="about"
+                fill
+                className="object-cover rounded-md"
               />
             </div>
 
-            <div className=" py-8 rounded-md sm:py-12 px-5 sm:px-12 md:px-8 lg:pr-12 lg:pl-16 2xl:pl-[160px] bg-[#F8F8F9] ">
-              <div className="lg:max-w-[600px] rounded-3xl text-center ">
-                <h3 className="font-medium leading-none mt-3  text-2xl md:text-3xl">
+            {/* Text content */}
+            <div className="py-8 rounded-md sm:py-12 px-5 sm:px-12 md:px-8 lg:pr-12 lg:pl-16 2xl:pl-[160px] bg-[#F8F8F9]">
+              <div className="lg:max-w-[600px] rounded-3xl text-center">
+                <h3 className="font-medium leading-none text-2xl md:text-3xl">
                   About Us
                 </h3>
-                <p className="mt-3 rounded-md text-base sm:text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-                  aliquid? Corporis, ex ratione voluptates, id porro quibusdam
-                  architecto neque cupiditate perspiciatis quis aliquid deleniti
-                  incidunt, explicabo ea sapiente ab? Quidem corporis deserunt
-                  officia voluptas harum ipsum quas in veritatis veniam
-                  officiis, eveniet totam distinctio quasi velit sint id
-                  aspernatur facilis!
+                <p className="mt-3 text-base sm:text-lg">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Pariatur nam illum asperiores dolore deleniti in libero
+                  consectetur soluta? Error ab dolores corrupti accusantium
+                  maiores molestias doloribus fuga enim facilis sapiente
+                  perferendis accusamus, explicabo inventore tempore qui libero.
+                  Repudiandae, quae tenetur?
                 </p>
                 <p className="mt-3 text-base sm:text-lg">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Consequuntur, iusto necessitatibus ipsa praesentium quis
-                  voluptatem incidunt veniam pariatur nulla cumque nisi placeat
-                  et officiis omnis quas excepturi. Necessitatibus doloremque
-                  beatae fugit repellendus mollitia earum? Facilis distinctio
-                  qui recusandae dolor architecto amet in sequi hic culpa,
-                  blanditiis neque, quae velit voluptas ipsam! Praesentium quam
-                  blanditiis voluptatum beatae.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam aspernatur earum dicta, deserunt ex facilis, maxime
+                  debitis, vero maiores ullam atque reiciendis temporibus est!
+                  Nesciunt ad repellat quidem. Illum impedit accusamus
+                  perferendis corrupti rem perspiciatis consectetur atque
+                  distinctio doloremque quisquam.
                 </p>
               </div>
             </div>
