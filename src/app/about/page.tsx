2c6ac4916ecdc1e5f5f23fc19ba328d91ff6221e
vaqsii <vaqsii23@gmail.com>
2025-06-08 22:11:21 +0400

    uppercase

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ function page() {
           
             <div className="py-8 rounded-md sm:py-12 px-5 sm:px-12 md:px-8 lg:pr-12 lg:pl-16 2xl:pl-[160px] bg-[#F8F8F9]">
               <div className="lg:max-w-[600px] rounded-3xl text-center">
-                <h3 className="font-medium leading-none text-2xl md:text-3xl">
+                <h3 className="font-medium uppercase leading-none text-2xl md:text-3xl">
                   About Us
                 </h3>
                 <p className="mt-3 text-base sm:text-lg">
